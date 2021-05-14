4e27a0c53111e0230c42eeadd005128b1ecd1809
Damy Metzke <damy.metzke@gmail.com>
2021-05-14 18:34:42 +0200

    Add Model

---
+++ App/Core/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use \App\Core\DebugHandler;
+
+abstract class Model
+{
+    abstract protected function serialize(): array;
+    abstract public function deSerialize(array $data): void;
+
+    final public function store()
+    {
+        DebugHandler::getInstance()->logMessage("info", json_encode($this->serialize()));
+    }
+}
--- Index.php
+++ Index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Core\DebugHandler;
+use App\Core\Model;
 
 spl_autoload_register(
     function ($className) {
@@ -11,5 +12,32 @@ spl_autoload_register(
 );
 
 DebugHandler::getInstance()->startup();
+
+class DemoModel extends Model
+{
+    protected function serialize(): array
+    {
+        return [
+            "a" => $this->a,
+            "b" => $this->b,
+        ];
+    }
+
+    public function deSerialize(array $data): void
+    {
+        $this->a = $data["a"];
+        $this->b = $data["b"];
+    }
+
+    private int $a;
+    private int $b;
+}
+
+$demo = new DemoModel();
+$demo->deSerialize([
+    "a" => 10,
+    "b" => 20,
+]);
+$demo->store();
 
 include "Routes.php";
