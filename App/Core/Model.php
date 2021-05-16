829ee94c46cd7fe32c90c6a5b43a24726f8c6b46
Damy Metzke <damy.metzke@gmail.com>
2021-05-16 21:30:52 +0200

    Document `Model` class

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -7,11 +7,26 @@ namespace App\Core;
 use \App\Core\DebugHandler;
 use \App\Core\RelationManager;
 
+/**
+ * Base class for all models.
+ * 
+ * A model is responsible for all data of the application.
+ * This data is stored in an SQL database.
+ * 
+ * Some variations on MVC connect the model to the view.
+ * This is **not** the case for this project.
+ * 
+ * @see `Docs/Example.md` for a concrete example including how to use models.
+ * @see https://zeekat.nl/articles/mvc-for-the-web.html.
+ */
 abstract class Model
 {
     abstract protected function serialize(): array;
     abstract public function deSerialize(array $data): void;
 
+    /**
+     * Store current state in the database.
+     */
     final public function store()
     {
         DebugHandler::getInstance()->logMessage("info", json_encode($this->serialize()));
@@ -22,6 +37,9 @@ abstract class Model
         return json_encode($this->serialize());
     }
 
+    /**
+     * @see `Docs/Relations.md`.
+     */
     final protected function relationFromKey(string $property, string $referencedTable, string $id, string $key, callable $generator): Model
     {
         if ($this->$property === null) {
@@ -31,6 +49,9 @@ abstract class Model
         return $this->$property;
     }
 
+    /**
+     * @see `Docs/Relations.md`.
+     */
     final protected function relationOne(string $property, string $referenceTable, string $id, string $key, callable $generator): Model
     {
         if ($this->$property === null) {
@@ -40,6 +61,9 @@ abstract class Model
         return $this->$property;
     }
 
+    /**
+     * @see `Docs/Relations.md`.
+     */
     final protected function relationMany(string $property, string $referenceTable, string $id, string $key, callable $generator): array
     {
         if ($this->$property === null) {
