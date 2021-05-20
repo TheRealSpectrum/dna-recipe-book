bf0f0ede31d0d3a0ee9c1dcdf93f978aadfd5f3f
Damy Metzke <damy.metzke@gmail.com>
2021-05-20 15:23:26 +0200

    Add `Response`

---
+++ App/Core/Response/Response.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Response;
+
+use \App\Core\DebugHandler;
+
+abstract class Response
+{
+    final public function apply(): void
+    {
+        $mode = getenv("APP_MODE");
+        if ($mode === false) {
+            $mode = "PRODUCTION";
+        }
+
+        $mode = strtoupper($mode);
+
+        if ($mode === "DEVELOPMENT") {
+            $this->applyDevelopment();
+        } else if ($mode === "PRODUCTION") {
+            $this->applyProduction();
+        } else {
+            DebugHandler::getInstance()->logMessage("WARNING", "Unrecognized application mode: '$mode'");
+            $this->applyProduction();
+        }
+    }
+
+    abstract public function applyProduction(): void;
+    abstract public function applyDevelopment(): void;
+}
