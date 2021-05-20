fba0aa917ab02c3f0a05482de76f05e54a72b6f1
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-20 14:50:14 +0200

    Write routes for Recipes.

--- Routes.php
+++ Routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\AuthController;
 use \App\Controllers\Home;
 use App\Controllers\RecipesController;
 use \App\Core\Route;
@@ -9,5 +10,13 @@ $recipesController = new RecipesController();
 
 Route::run([
     Route::get("/", [RecipesController::class, "index"]),
+    Route::get("/recipes/create", [RecipesController::class, "create"]),
+    Route::post("/recipes", [RecipesController::class, "store"]),
     Route::get("/recipes/:", [RecipesController::class, "show"]),
+    Route::get("/recipes/:/edit", [RecipesController::class, "edit"]),
+    Route::put("/recipes/:", [RecipesController::class, "update"]),
+    Route::delete("/recipes/:", [RecipesController::class, "destroy"]),
+
+    Route::get("/login", [AuthController::class, "login"]),
+    Route::get("/logout", [AuthController::class, "logout"]),
 ]);
