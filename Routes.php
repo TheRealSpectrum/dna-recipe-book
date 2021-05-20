e6b0f384118d64f6a75494430ad08e2dad6dbf79
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-20 15:06:12 +0200

    Edit RecipesController to RecipeController

--- Routes.php
+++ Routes.php
@@ -2,21 +2,22 @@
 
 use App\Controllers\AuthController;
 use \App\Controllers\Home;
-use App\Controllers\RecipesController;
+use App\Controllers\RecipeController;
 use \App\Core\Route;
 
-$homeController = new Home();
-$recipesController = new RecipesController();
+
 
 Route::run([
-    Route::get("/", [RecipesController::class, "index"]),
-    Route::get("/recipes/create", [RecipesController::class, "create"]),
-    Route::post("/recipes", [RecipesController::class, "store"]),
-    Route::get("/recipes/:", [RecipesController::class, "show"]),
-    Route::get("/recipes/:/edit", [RecipesController::class, "edit"]),
-    Route::patch("/recipes/:", [RecipesController::class, "update"]),
-    Route::delete("/recipes/:", [RecipesController::class, "destroy"]),
+    Route::get("/", [RecipeController::class, "index"]),
+    Route::get("/recipes/create", [RecipeController::class, "create"]),
+    Route::post("/recipes", [RecipeController::class, "store"]),
+    Route::get("/recipes/:", [RecipeController::class, "show"]),
+    Route::get("/recipes/:/edit", [RecipeController::class, "edit"]),
+    Route::patch("/recipes/:", [RecipeController::class, "update"]),
+    Route::delete("/recipes/:", [RecipeController::class, "destroy"]),
 
     Route::get("/login", [AuthController::class, "login"]),
     Route::get("/logout", [AuthController::class, "logout"]),
+
+
 ]);
