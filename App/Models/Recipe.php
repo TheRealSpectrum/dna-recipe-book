4b8a725d8f35caf530509228389a60a2d19f5f97
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-21 10:29:49 +0200

    Add ingredient Model

--- App/Models/Recipe.php
+++ App/Models/Recipe.php
@@ -4,6 +4,14 @@ use App\Core\Model;
 
 final class Recipe extends Model 
 {
-    protected array $columns = ["id", "title", "description", "author", "preparation_time", "cooking_time", "servings", "image"]; 
+    protected array $columns = ["id", "title", "description", "author_id", "preparation_time", "cooking_time", "servings", "image"]; 
     protected string $table = "recipes";
+
+
+    // Relation functions
+
+
+    protected ?Recipe $referencedRelation = null;
+    protected ?Recipe $oneRelation = null;
+    protected ?array $manyRelation = null;
 }
