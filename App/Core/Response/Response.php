b5cb53925f10e038b712725a8e60626c6facf629
Damy Metzke <damy.metzke@gmail.com>
2021-05-25 10:15:56 +0200

    Add support for `.env` file.

--- App/Core/Response/Response.php
+++ App/Core/Response/Response.php
@@ -5,24 +5,20 @@ declare(strict_types=1);
 namespace App\Core\Response;
 
 use \App\Core\DebugHandler;
+use \App\Core\Environment;
 
 abstract class Response
 {
     final public function apply(): void
     {
-        $mode = getenv("APP_MODE");
-        if ($mode === false) {
-            $mode = "PRODUCTION";
-        }
-
-        $mode = strtoupper($mode);
+        $mode = strtoupper(Environment::getInstance()->getEnvironment("APP_MODE", "PRODUCTION"));
 
         if ($mode === "DEVELOPMENT") {
             $this->applyDevelopment();
         } else if ($mode === "PRODUCTION") {
             $this->applyProduction();
         } else {
-            DebugHandler::getInstance()->logMessage("WARNING", "Unrecognized application mode: '$mode'");
+            DebugHandler::getInstance()->logMessage("WARNING", "Unrecognized application mode: '$mode'. Defaulting to 'PRODUCTION'.");
             $this->applyProduction();
         }
     }
