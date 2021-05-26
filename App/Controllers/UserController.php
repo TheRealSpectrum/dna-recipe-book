65c9aa3c577ce9350056497c3c5b8585c60a073b
Damy Metzke <damy.metzke@gmail.com>
2021-05-26 15:38:45 +0200

    Implement `UserController`

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 
 use \App\Core\Controller;
 use \App\Core\View;
+use \App\Core\Request;
+
+use \App\Core\Response\RedirectResponse;
 
 use \App\Models\User;
 
@@ -16,28 +19,67 @@ class UserController extends Controller
             "users" => User::query("SELECT * FROM `users`"),
         ], "Main");
     }
+
     function create()
     {
         return View::view("User/CreateUser", [], "Main");
     }
-    function store()
+    function store(Request $request)
+
     {
-        // 
+        $newUser = User::create([
+            "name" => $request->getParameter("name"),
+            "password" => password_hash($request->getParameter("password"), PASSWORD_BCRYPT),
+            "isAdmin" => $request->getParameter("isAdmin") === "1" ? 1 : 0,
+        ]);
+
+        $newUser->store();
+
+        return new RedirectResponse("/users");
     }
-    function show()
+
+    function show($id)
     {
-        return View::view("User/ShowUser", [], "Main");
+        return View::view("User/ShowUser", [
+            "user" => User::query("SELECT * FROM `users` WHERE `id` = $id LIMIT 1")[0],
+        ], "Main");
     }
-    function edit()
+
+    function edit($id)
     {
-        return View::view("User/EditUser", [], "Main");
+        return View::view("User/EditUser", [
+            "user" => User::query("SELECT * FROM `users` WHERE `id` = $id LIMIT 1")[0],
+        ], "Main");
     }
-    function update()
+
+    function update(Request $request, $id)
     {
-        // 
+        $toEdit = User::query("SELECT * FROM `users` WHERE `id` = $id LIMIT 1")[0];
+
+        $name = $request->getParameter("name");
+        $password = $request->getParameter("password");
+        $isAdmin = $request->getParameter("isAdmin");
+
+        if ($name !== null) {
+            $toEdit->name = $name;
+        }
+
+        if ($password !== null) {
+            $toEdit->password = password_hash($password, PASSWORD_BCRYPT);
+        }
+
+        if ($isAdmin !== null) {
+            $toEdit->isAdmin = $isAdmin === "1" ? 1 : 0;
+        }
+
+        $toEdit->store();
+
+        return new RedirectResponse("/users/{$toEdit->id}");
     }
-    function destroy()
+
+    function destroy($id)
     {
-        return View::view("User/DeleteUser", [], "Main");
+        User::query("DELETE FROM `users` WHERE id = $id");
+        return new RedirectResponse("/users");
     }
 }
--- Views/User/EditUser.php
+++ Views/User/EditUser.php
@@ -1,1 +1,1 @@
-Test - EditUser
+<?= $user ?>
