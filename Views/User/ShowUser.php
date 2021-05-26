4023cd709aef5f2e8c8ad918a183559e27e0b53a
Damy Metzke <damy.metzke@gmail.com>
2021-05-26 17:04:06 +0200

    Write `User/ShowUser` view

--- Views/User/ShowUser.php
+++ Views/User/ShowUser.php
@@ -1,1 +1,24 @@
-<?= $user ?>
+<?php
+
+declare(strict_types=1);
+
+use \App\Core\View;
+
+?>
+
+<section class="text-gray-400 body-font bg-gray-900">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex w-full mb-20 flex-col gap-3">
+            <?= View::component("User/ShowRow", [
+                "label" => "Name",
+                "content" => $user->name,
+            ]) ?>
+
+            <?= View::component("User/ShowRow", [
+                "label" => "Type",
+                "content" => $user->isAdmin ? "Admin" : "User",
+            ]) ?>
+
+        </div>
+    </div>
+</section>
