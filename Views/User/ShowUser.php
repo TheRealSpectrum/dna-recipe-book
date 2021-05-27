3d6d4fb005acca09bf65a190e7400d1384c74f0c
Damy Metzke <damy.metzke@gmail.com>
2021-05-27 10:14:39 +0200

    Add authored recipes to user show

--- Views/User/ShowUser.php
+++ Views/User/ShowUser.php
@@ -7,7 +7,7 @@ use \App\Core\View;
 ?>
 
 <section class="text-gray-400 body-font bg-gray-900">
-    <div class="container px-5 py-24 mx-auto">
+    <div class="container px-5 pt-24 pb-5 mx-auto">
         <div class="flex w-full mb-20 flex-col gap-3">
             <?= View::component("User/ShowRow", [
                 "label" => "Name",
@@ -20,5 +20,15 @@ use \App\Core\View;
             ]) ?>
 
         </div>
+    </div>
+    <h2 class="container px-5 pt-5 text-3xl mx-auto">Recipes</h2>
+    <div class="container px-5 pt-5 pb-24 mx-auto">
+        <?php
+        foreach ($user->recipes() as $recipe) {
+            echo View::component("User/ShowRecipe", [
+                "recipe" => $recipe,
+            ]);
+        }
+        ?>
     </div>
 </section>
