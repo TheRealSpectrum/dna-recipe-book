7fdde843a25cfcd7d172312cf164e2fa37f9f429
Damy Metzke <damy.metzke@gmail.com>
2021-05-27 15:30:02 +0200

    Update `Main` layout

--- Layouts/Main.php
+++ Layouts/Main.php
@@ -11,7 +11,7 @@
     <title>Blueprint Cooking</title>
 </head>
 
-<body>
+<body class="min-h-screen">
     <!-- Header -->
     <div class="flex flex-col h-screen">
         <header class="text-gray-400 bg-gray-800 body-font">
@@ -24,9 +24,9 @@
                 </a>
                 <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                     <a href="/" class="mr-5 hover:text-white">Home</a>
-                    <a href="#" class="mr-5 hover:text-white">Recipes</a>
-                    <a href="#" class="mr-5 hover:text-white">Third Link</a>
-                    <a href="#" class="mr-5 hover:text-white">Fourth Link</a>
+                    <a href="/recipes" class="mr-5 hover:text-white">Recipes</a>
+                    <a href="/categories" class="mr-5 hover:text-white">Categories</a>
+                    <a href="/users" class="mr-5 hover:text-white">Users</a>
                 </nav>
                 <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
@@ -36,90 +36,13 @@
             </div>
         </header>
 
-        <?= $yield() ?>
+        <main class="flex-grow">
+            <?= $yield() ?>
+        </main>
 
-        <footer class="text-gray-400 bg-gray-800 body-font">
-            <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-                <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-                    <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span class="ml-3 text-xl">Tailblocks</span>
-                    </a>
-                    <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
-                </div>
-                <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-                        <nav class="list-none mb-10">
-                            <li>
-                                <a class="text-gray-400 hover:text-white">First Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Second Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Third Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Fourth Link</a>
-                            </li>
-                        </nav>
-                    </div>
-                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-                        <nav class="list-none mb-10">
-                            <li>
-                                <a class="text-gray-400 hover:text-white">First Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Second Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Third Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Fourth Link</a>
-                            </li>
-                        </nav>
-                    </div>
-                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-                        <nav class="list-none mb-10">
-                            <li>
-                                <a class="text-gray-400 hover:text-white">First Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Second Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Third Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Fourth Link</a>
-                            </li>
-                        </nav>
-                    </div>
-                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-                        <nav class="list-none mb-10">
-                            <li>
-                                <a class="text-gray-400 hover:text-white">First Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Second Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Third Link</a>
-                            </li>
-                            <li>
-                                <a class="text-gray-400 hover:text-white">Fourth Link</a>
-                            </li>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+        <footer class="text-gray-400 bg-gray-800 body-font px-10 py-5 flex flex-row gap-10 justify-around">
+            <span>&copy; Damy Metzke, Niels van der Linden, 2021</span>
+            <a href="https://github.com/TheRealSpectrum/dna-recipe-book">Github: https://github.com/TheRealSpectrum/dna-recipe-book</a>
         </footer>
     </div>
 </body>
