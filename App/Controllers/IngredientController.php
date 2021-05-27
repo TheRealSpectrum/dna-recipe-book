9126dbbffaa1f370f56d6e8a93d28f9cd2dffa94
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-27 22:03:54 +0200

    Add ingredient form to edit recipe

--- App/Controllers/IngredientController.php
+++ App/Controllers/IngredientController.php
@@ -1,5 +1,7 @@
 <?php 
 
+declare(strict_types=1);
+
 namespace App\Controllers;
 
 use \App\Core\Controller;
@@ -11,13 +13,16 @@ class IngredientController extends Controller {
     
     public function store(Request $request) 
     {
+        $recipe_id = $request->getParameter("recipe_id");
         $newIngredient = Ingredient::create([
             "name" => $request->getParameter("ingredient_title"),
             "quantity" => $request->getParameter("ingredient_quantity"),
-            "recipe_id" => 
+            "recipe_id" => $recipe_id,
         ]);
 
         $newIngredient->store();
+
+        return new RedirectResponse("/recipes/$recipe_id/edit/");
 
     }
 
--- Views/Recipe/EditRecipe.php
+++ Views/Recipe/EditRecipe.php
@@ -7,11 +7,11 @@ declare(strict_types=1);
 <!-- Parts of code are copyrighted 2020 Mert Cukuren. Code released under the MIT license. See ThirdParty/Licenses/Tailblocks -->
 <!-- Content -->
 <section class="flex-grow text-gray-400 body-font bg-gray-900">
-    <a href="/recipes">
-        <p class="container px-5 pt-5 text-xl mx-auto">Back to recipes without saving</p>
-    </a>
+  <a href="/recipes">
+    <p class="container px-5 pt-5 text-xl mx-auto">Back to recipes without saving</p>
+  </a>
   <form class="mx-auto w-full max-w-lg my-4" method="POST" action="/recipes/<?= $recipe->id ?>">
-  <input type="hidden" name="_method" value="PATCH">
+    <input type="hidden" name="_method" value="PATCH">
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full md:w-100 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="recipe_title">
@@ -96,20 +96,36 @@ declare(strict_types=1);
     </div>
   </form>
   <form class="mx-auto w-full max-w-lg my-4" method="POST" action="/ingredients">
-  <div class="flex flex-wrap -mx-3 mb-6">
-  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-    <label for="ingredient_title">Ingredient title</label>
-    <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2 mb-3 leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_title">
-    </div>
-  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-    <label for="ingredient_title">Ingredient Quantity</label>
-    <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2 mb-3 leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_quantity">
-    </div>
-    <div class="px-4 py-3  text-right">
+    <div class="flex flex-wrap -mx-3 mb-2">
+      <div class="w-full md:w-1/2 px-3  md:mb-0">
+        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient name</label>
+        <input type="text" name="recipe_id" value="<?= $ingredients[0]->recipe_id ?>" hidden>
+        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_title">
+      </div>
+      <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient Quantity</label>
+        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_quantity">
+      </div>
+      <div class="px-4 py-3  text-right">
         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Add ingredient
         </button>
       </div>
-  </div>
+    </div>
+    <div class="flex flex-wrap -mx-3 mb-2">
+      <div class="w-full md:w-1/2 px-3 md:mb-0">
+          
+        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient name</label>
+        <input type="text" name="recipe_id" value="<?= $ingredients[0]->recipe_id ?>" hidden>
+        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_title">
+        
+      </div>
+      <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient Quantity</label>
+        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_quantity">
+      </div>
+    </div>
+
+
   </form>
 </section>
