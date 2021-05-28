e052418c8a22e2fe4fb0996cd4c8801a4335e5c0
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-28 10:30:51 +0200

    Fix typo

--- Views/Recipe/EditRecipe.php
+++ Views/Recipe/EditRecipe.php
@@ -114,11 +114,8 @@ declare(strict_types=1);
     </div>
     <div class="flex flex-wrap -mx-3 mb-2">
       <div class="w-full md:w-1/2 px-3 md:mb-0">
-          
         <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient name</label>
-        <input type="text" name="recipe_id" value="<?= $ingredients[0]->recipe_id ?>" hidden>
         <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_title">
-        
       </div>
       <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient Quantity</label>
