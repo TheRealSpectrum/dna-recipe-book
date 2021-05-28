129b02d40c12b19a7208fe2062f08cfcceebe3dd
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-28 19:19:58 +0200

    Add Ingredient CreateRow component

--- Views/Recipe/EditRecipe.php
+++ Views/Recipe/EditRecipe.php
@@ -12,32 +12,18 @@ use App\Core\View;
   <a href="/recipes">
     <p class="container px-5 pt-5 text-xl mx-auto">Back to recipes without saving</p>
   </a>
-  <?php
-    echo View::component("Recipe/EditRow", [
-      "recipe" => $recipe,
-      "authors" => $authors,
-      "categories" => $categories,
-      "recipe_categories" => $recipe_categories
-    ]);
-  ?>
-  <form class="mx-auto w-full max-w-lg my-4" method="POST" action="/ingredients">
-    <div class="flex flex-wrap -mx-3 mb-2">
-      <div class="w-full md:w-1/2 px-3  md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient name</label>
-        <input type="text" name="recipe_id" value="<?= $ingredients[0]->recipe_id ?>" hidden>
-        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_title">
-      </div>
-      <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-        <label class="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2" for="ingredient_title">Ingredient Quantity</label>
-        <input class="block w-full bg-gray-200 text-gray-700 border rounded py-2 px-2  leading-tight text-sm focus:outline-none focus:bg-white" type="text" name="ingredient_quantity">
-      </div>
-      <div class="px-4 py-3  text-right">
-        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          Add ingredient
-        </button>
-      </div>
-    </div>
-  </form>
+
+  <?= View::component("Recipe/EditRow", [
+    "recipe" => $recipe,
+    "authors" => $authors,
+    "categories" => $categories,
+    "recipe_categories" => $recipe_categories
+  ]) ?>
+
+  <?= View::component("Ingredient/CreateRow", [
+    "ingredients" => $ingredients
+  ]) ?>
+
   <div class="mx-auto w-full max-w-2xl my-4">
     <div class="flex flex-wrap -mx-3 mb-2">
       <?php
