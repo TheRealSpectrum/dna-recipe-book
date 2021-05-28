55aeafbf71cfdcbb8a9d840e6a1c96f10e16727c
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-28 19:25:52 +0200

    Clean up Ingredient model

--- App/Models/Ingredient.php
+++ App/Models/Ingredient.php
@@ -10,13 +10,5 @@ final class Ingredient extends Model
 {
     protected array $columns = ["id", "recipe_id", "name", "quantity"]; 
     protected string $table = "ingredients";
-
     
-
-    // Relation functions
-
-
-    protected ?Recipe $referencedRelation = null;
-    protected ?Recipe $oneRelation = null;
-    protected ?array $manyRelation = null;
 }
