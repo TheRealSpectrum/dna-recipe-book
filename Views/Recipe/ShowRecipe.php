8e4c16f3a2c77bde1ca451a490e81dd3dde7042c
niels-linden <60019402+niels-linden@users.noreply.github.com>
2021-05-28 21:21:08 +0200

    Fix recipe view styling

--- Views/Recipe/ShowRecipe.php
+++ Views/Recipe/ShowRecipe.php
@@ -5,7 +5,7 @@ use App\Core\View;
 <!-- Parts of code are copyrighted 2020 Mert Cukuren. Code released under the MIT license. See ThirdParty/Licenses/Tailblocks -->
 
 <!-- Content -->
-<section class="text-gray-400 bg-gray-900 body-font">
+<section class="text-gray-400 bg-gray-900 body-font min-h-full">
   <div class="container px-5 py-24 mx-auto flex flex-col">
     <?= View::component("Recipe/ShowRow", [
       "title" => $recipe->title,
